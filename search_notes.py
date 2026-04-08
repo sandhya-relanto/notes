b1c113fe86460d55c5aca8c1236cd24db399e8f5
sandhya-relanto <sandhya.ranie@relanto.ai>
2026-04-08 15:49:45 +0530

    Fixed search_notes error

--- search_notes.py
+++ search_notes.py
@@ -1,13 +1,20 @@
 def search_notes():
     keyword = input("Enter keyword to search: ")
+
     try:
         with open("notes.txt", "r") as file:
             notes = file.readlines()
             found = False
+
             print("\n🔎 Search Results:")
+
             for note in notes:
                 if keyword.lower() in note.lower():
                     print("- " + note.strip())
                     found = True
+
             if not found:
                 print("No matching notes found.")
+
+    except FileNotFoundError:
+        print("No notes found yet.")
